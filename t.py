3ac2cc3d0269ebc9c930220bc292969d15f11ec6
supperfangamm3 <80982012+supperfangamm3@users.noreply.github.com>
2023-01-26 23:08:37 -0500

    t

--- t.py
+++ t.py
@@ -1,5 +1,5 @@
 #Useless Python code:
-num = int(input("Type a number, and i'll duplicate it by the number of your choice"))
+num = int(input("Type a number, and i'll multiply it by the number of your choice"))
 num2 = int(input("Type a second number to finish."))
 
 print("Got result: ", num*num2)
